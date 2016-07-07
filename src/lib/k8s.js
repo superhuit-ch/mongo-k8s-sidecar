79cbc03a83093f3c7350e38f771104b89c80d09f
Yvan da Silva <yvan@superhuit.ch>
2016-07-07 22:41:12 +0200

    Clean up all the js files. Adds support for GlusterFS. Adds support for namespaces. Adds DB version const setting in makefile.

--- src/lib/k8s.js
+++ src/lib/k8s.js
@@ -4,50 +4,53 @@ var util = require("util");
 
 fs = require('fs');
 
-var readToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token');
+var readToken = fs.readFileSync(
+	'/var/run/secrets/kubernetes.io/serviceaccount/token'
+);
 
 var client = new Client({
-  host:  config.kubernetesROServiceAddress,
-  protocol: 'https',
-  version: 'v1',
-  token: readToken
+	host:  config.kubernetesROServiceAddress,
+	protocol: 'https',
+	version: 'v1',
+	token: readToken
 });
 
 var getMongoPods = function getPods(done) {
-  client.pods.get(function (err, podResult) {
-    if (err) {
-      return done(err);
-    }
-    var pods = [];
-    for (var j in podResult) {
-      pods = pods.concat(podResult[j].items)
-    }
-    var labels = config.mongoPodLabelCollection;
-    var results = [];
-    for (var i in pods) {
-      var pod = pods[i];
-      if (podContainsLabels(pod, labels)) {
-        results.push(pod);
-      }
-    }
+	client.pods.get(function (err, podResult) {
+		if (err) {
+			return done(err);
+		}
+		var pods = [];
+		for (var j in podResult) {
+			pods = pods.concat(podResult[j].items)
+		}
+		var labels = config.mongoPodLabelCollection;
+		var results = [];
+		for (var i in pods) {
+			var pod = pods[i];
+			if (podContainsLabels(pod, labels)) {
+				results.push(pod);
+			}
+		}
 
-    done(null, results);
-  });
+		done(null, results);
+	});
 };
 
 var podContainsLabels = function podContainsLabels(pod, labels) {
-  if (!pod.metadata || !pod.metadata.labels) return false;
+	if (!pod.metadata || !pod.metadata.labels) return false;
 
-  for (var i in labels) {
-    var kvp = labels[i];
-    if (!pod.metadata.labels[kvp.key] || pod.metadata.labels[kvp.key] != kvp.value) {
-      return false;
-    }
-  }
+	for (var i in labels) {
+		var kvp = labels[i];
+		if (!pod.metadata.labels[kvp.key] ||
+		    pod.metadata.labels[kvp.key] != kvp.value) {
+			return false;
+		}
+	}
 
-  return true;
+	return true;
 };
 
 module.exports = {
-  getMongoPods: getMongoPods
+	getMongoPods: getMongoPods
 };
--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -5,163 +5,171 @@ var async = require('async');
 var localhost = '127.0.0.1'; //Can access mongo as localhost from a sidecar
 
 var getDb = function(host, done) {
-  //If they called without host like getDb(function(err, db) { ... });
-  if (arguments.length === 1) {
-    if (typeof arguments[0] === 'function') {
-      done = arguments[0];
-      host = localhost;
-    }
-    else {
-      throw new Error('getDb illegal invocation. User either getDb(\'hostAddr\', function(err, db) { ... }) OR getDb(function(err, db) { ... })');
-    }
-  }
+	//If they called without host like getDb(function(err, db) { ... });
+	if (arguments.length === 1) {
+		if (typeof arguments[0] === 'function') {
+			done = arguments[0];
+			host = localhost;
+		}
+		else {
+			throw new Error(
+				'getDb illegal invocation. User either getDb(\'hostAddr\', function(err, db) { ... }) OR getDb(function(err, db) { ... })'
+			);
+		}
+	}
 
-  host = host || localhost;
-  var mongoDb = new Db('local', new MongoServer(host, 27017));
-  mongoDb.open(function (err, db) {
-    if (err) {
-      return done(err);
-    }
+	host = host || localhost;
+	var mongoDb = new Db('local', new MongoServer(host, 27017));
+	mongoDb.open(function (err, db) {
+		if (err) {
+			return done(err);
+		}
 
-    return done(null, db);
-  });
+		return done(null, db);
+	});
 };
 
 var replSetGetConfig = function(db, done) {
-  db.admin().command({ replSetGetConfig: 1 }, {}, function (err, results) {
-    if (err) {
-      return done(err);
-    }
+	db.admin().command({ replSetGetConfig: 1 }, {},
+	                   function (err, results) {
+		if (err) {
+			return done(err);
+		}
 
-    return done(null, results.config);
-  });
+		return done(null, results.config);
+	});
 };
 
 var replSetGetStatus = function(db, done) {
-  db.admin().command({ replSetGetStatus: {} }, {}, function (err, results) {
-    if (err) {
-      return done(err);
-    }
+	db.admin().command({ replSetGetStatus: {} }, {},
+	                   function (err, results) {
+		if (err) {
+			return done(err);
+		}
 
-    return done(null, results);
-  });
+		return done(null, results);
+	});
 };
 
 var initReplSet = function(db, hostIpAndPort, done) {
-  console.log('initReplSet', hostIpAndPort);
+	console.log('initReplSet', hostIpAndPort);
 
-  db.admin().command({ replSetInitiate: {} }, {}, function (err) {
-    if (err) {
-      return done(err);
-    }
+	db.admin().command({ replSetInitiate: {} }, {},
+	                   function (err) {
+		if (err) {
+			return done(err);
+		}
 
-    //We need to hack in the fix where the host is set to the hostname which isn't reachable from other hosts
-    replSetGetConfig(db, function(err, config) {
-      if (err) {
-        return done(err);
-      }
+		//We need to hack in the fix where the host is set to the hostname which isn't reachable from other hosts
+		replSetGetConfig(db, function(err, config) {
+			if (err) {
+				return done(err);
+			}
 
-      config.members[0].host = hostIpAndPort;
-      async.retry({times: 20, interval: 500}, function(callback) {
-        replSetReconfig(db, config, false, callback);
-      }, function(err, results) {
-        if (err) {
-          return done(err);
-        }
+			config.members[0].host = hostIpAndPort;
+			async.retry({times: 20, interval: 500}, function(callback) {
+				replSetReconfig(db, config, false, callback);
+			}, function(err, results) {
+				if (err) {
+					return done(err);
+				}
 
-        return done();
-      });
-    });
-  });
+				return done();
+			});
+		});
+	});
 };
 
 var replSetReconfig = function(db, config, force, done) {
-  console.log('replSetReconfig', config);
+	console.log('replSetReconfig', config);
 
-  config.version++;
+	config.version++;
 
-  db.admin().command({ replSetReconfig: config, force: force }, {}, function (err) {
-    if (err) {
-      return done(err);
-    }
+	db.admin().command({ replSetReconfig: config, force: force },
+	                   {}, function (err) {
+		if (err) {
+			return done(err);
+		}
 
-    return done();
-  });
+		return done();
+	});
 };
 
-var addNewReplSetMembers = function(db, addrToAdd, addrToRemove, shouldForce, done) {
-  replSetGetConfig(db, function(err, config) {
-    if (err) {
-      return done(err);
-    }
+var addNewReplSetMembers = function(db, addrToAdd,
+                                    addrToRemove, shouldForce,
+                                    done) {
+	replSetGetConfig(db, function(err, config) {
+		if (err) {
+			return done(err);
+		}
 
-    addNewMembers(config, addrToAdd);
+		addNewMembers(config, addrToAdd);
 
-    removeDeadMembers(config, addrToRemove);
+		removeDeadMembers(config, addrToRemove);
 
-    replSetReconfig(db, config, shouldForce, done);
-  });
+		replSetReconfig(db, config, shouldForce, done);
+	});
 };
 
 var addNewMembers = function(config, addrsToAdd) {
-  if (!addrsToAdd || !addrsToAdd.length) return;
+	if (!addrsToAdd || !addrsToAdd.length) return;
 
-  //Follows what is basically in mongo's rs.add function
-  var max = 0;
+	//Follows what is basically in mongo's rs.add function
+	var max = 0;
 
-  for (var j in config.members) {
-    if (config.members[j]._id > max) {
-      max = config.members[j]._id;
-    }
-  }
+	for (var j in config.members) {
+		if (config.members[j]._id > max) {
+			max = config.members[j]._id;
+		}
+	}
 
-  for (var i in addrsToAdd) {
-    var cfg = {
-      _id: ++max,
-      host: addrsToAdd[i]
-    };
+	for (var i in addrsToAdd) {
+		var cfg = {
+			_id: ++max,
+			host: addrsToAdd[i]
+		};
 
-    config.members.push(cfg);
-  }
+		config.members.push(cfg);
+	}
 };
 
 var removeDeadMembers = function(config, addrsToRemove) {
-  if (!addrsToRemove || !addrsToRemove.length) return;
+	if (!addrsToRemove || !addrsToRemove.length) return;
 
-  for (var i in addrsToRemove) {
-    var addrToRemove = addrsToRemove[i];
-    for (var j in config.members) {
-      var member = config.members[j];
-      if (member.host === addrToRemove) {
-        config.members.splice(j, 1);
-        break;
-      }
-    }
-  }
+	for (var i in addrsToRemove) {
+		var addrToRemove = addrsToRemove[i];
+		for (var j in config.members) {
+			var member = config.members[j];
+			if (member.host === addrToRemove) {
+				config.members.splice(j, 1);
+				break;
+			}
+		}
+	}
 };
 
 var isInReplSet = function(ip, done) {
-  getDb(ip, function(err, db) {
-    if (err) {
-      return done(err);
-    }
+	getDb(ip, function(err, db) {
+		if (err) {
+			return done(err);
+		}
 
-    replSetGetConfig(db, function(err, config) {
-      db.close();
-      if (!err && config) {
-        done(null, true);
-      }
-      else {
-        done(null, false);
-      }
-    });
-  });
+		replSetGetConfig(db, function(err, config) {
+			db.close();
+			if (!err && config) {
+				done(null, true);
+			}
+			else {
+				done(null, false);
+			}
+		});
+	});
 };
 
 module.exports = {
-  getDb: getDb,
-  replSetGetStatus: replSetGetStatus,
-  initReplSet: initReplSet,
-  addNewReplSetMembers: addNewReplSetMembers,
-  isInReplSet: isInReplSet
+	getDb: getDb,
+	replSetGetStatus: replSetGetStatus,
+	initReplSet: initReplSet,
+	addNewReplSetMembers: addNewReplSetMembers,
+	isInReplSet: isInReplSet
 };
